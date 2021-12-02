dd5dd96e9c7eb0c50d536b2d3efe575c744b4639
RileyMichael <riley.mcgee.13@gmail.com>
2021-12-01 22:37:53 -0600

    cleanup day 1

--- src/main/kotlin/day1/Day1.kt
+++ src/main/kotlin/day1/Day1.kt
@@ -3,16 +3,9 @@ package com.github.rileymichael.day1
 import com.github.rileymichael.Day
 
 object Day1: Day<Sequence<Int>>(1) {
-    override fun parseInput(input: Sequence<String>): Sequence<Int> {
-        return input.map(String::toInt)
-    }
+    override fun parseInput(input: Sequence<String>) = input.map(String::toInt)
 
-    override fun part1(input: Sequence<Int>): Any {
-        return input.zipWithNext().count { it.second > it.first }
-    }
+    override fun part1(input: Sequence<Int>) = input.zipWithNext().count { it.second > it.first }
 
-    override fun part2(input: Sequence<Int>): Any {
-        val windows = input.windowed(3).map { it.sum() }
-        return part1(windows)
-    }
+    override fun part2(input: Sequence<Int>) = part1(input.windowed(3).map(List<Int>::sum))
 }
