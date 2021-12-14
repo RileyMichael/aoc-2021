2679afd09ee9b8447060ee3ee8b27aa57d4a62a3
RileyMichael <riley.mcgee.13@gmail.com>
2021-12-13 18:07:51 -0600

    solve day 10 part 2

--- src/main/kotlin/day10/Day10.kt
+++ src/main/kotlin/day10/Day10.kt
@@ -19,19 +19,46 @@ object Day10 : Day<Sequence<String>>(10) {
             }
         }
         null
-    }.map(Char::score).reduce(Int::plus)
+    }.map(Char::corruptedScore).reduce(Int::plus)
 
-    override fun part2(input: Sequence<String>): Any {
-        TODO("Not yet implemented")
+    override fun part2(input: Sequence<String>): Long {
+        val completionStrings = input.mapNotNull line@{ line ->
+            val stack = mutableListOf<Char>()
+            line.forEach { c ->
+                when (c) {
+                    in openToClose.keys -> stack.add(c)
+                    else -> {
+                        val prev = stack.removeLast()
+                        if (openToClose[prev] != c) {
+                            return@line null
+                        }
+                    }
+                }
+            }
+            stack.toCompletionString()
+        }
+        return completionStrings.map {
+            it.map(Char::incompleteScore).reduce { acc, i -> acc * 5 + i }
+        }.toList().sorted().middle()
     }
 }
 
 private val openToClose = mapOf('(' to ')', '[' to ']', '{' to '}', '<' to '>')
+private fun MutableList<Char>.toCompletionString() = this.reversed().map { openToClose[it] }.joinToString("")
+private fun <T> List<T>.middle() = this.elementAt(this.size / 2)
 
-private fun Char.score() = when {
+private fun Char.corruptedScore() = when {
     this == ')' -> 3
     this == ']' -> 57
     this == '}' -> 1197
     this == '>' -> 25137
     else -> 0
+}
+
+private fun Char.incompleteScore() = when {
+    this == ')' -> 1L
+    this == ']' -> 2L
+    this == '}' -> 3L
+    this == '>' -> 4L
+    else -> 0L
 }
--- src/test/kotlin/day10/Day10Test.kt
+++ src/test/kotlin/day10/Day10Test.kt
@@ -1,6 +1,7 @@
 package com.github.rileymichael.day10
 
 import com.github.rileymichael.day10.Day10.part1
+import com.github.rileymichael.day10.Day10.part2
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 
@@ -15,6 +16,18 @@ class Day10Test {
     fun `part one solution`() {
         val actual = Day10.execute(::part1)
         Assertions.assertEquals(469755, actual)
+    }
+
+    @Test
+    fun `part two example`() {
+        val actual = Day10.execute(::part2, input)
+        Assertions.assertEquals(288957L, actual)
+    }
+
+    @Test
+    fun `part two solution`() {
+        val actual = Day10.execute(::part2)
+        Assertions.assertEquals(2762335572L, actual)
     }
 
     companion object {
