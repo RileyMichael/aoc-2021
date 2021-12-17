403e2ee2a3539cbee6ea3e61eb48bcdf59e1a98e
RileyMichael <riley.mcgee.13@gmail.com>
2021-12-16 23:16:33 -0600

    solve day 12 part 2

--- src/main/kotlin/day12/Day12.kt
+++ src/main/kotlin/day12/Day12.kt
@@ -12,23 +12,39 @@ object Day12 : Day<CaveSystem>(12) {
         }
     }
 
-    override fun part1(input: CaveSystem) = input.search().size
+    override fun part1(input: CaveSystem) = input.search(filter = part1Filter).size
 
-    override fun part2(input: CaveSystem): Any {
-        TODO("Not yet implemented")
-    }
+    override fun part2(input: CaveSystem) = input.search(filter = part2Filter).size
 }
 
 typealias Cave = String
 typealias CaveSystem = Map<Cave, Set<Cave>>
 typealias Path = List<Cave>
+typealias PathFilter = (Cave, Path) -> Boolean
+
+fun Cave.isBig() = all { it.isUpperCase() }
+
+val part1Filter = { cave: Cave, path: Path ->
+    cave.isBig() || cave !in path
+}
+
+val part2Filter = { cave: Cave, path: Path ->
+    if (cave == "start") {
+        false
+    } else {
+        cave.isBig() || cave !in path || path.filterNot(Cave::isBig).groupingBy { it }.eachCount().none { it.value > 1 }
+    }
+
+}
 
 private fun CaveSystem.search(
     cave: Cave = "start",
-    path: Path = listOf("start")
+    path: Path = listOf("start"),
+    filter: PathFilter
 ): Set<Path> = when (cave) {
     "end" -> setOf(path)
     else -> getOrDefault(cave, emptySet())
-        .filter { it.all(Char::isUpperCase) || it !in path }
-        .flatMap { search(it, path + it) }.toSet()
+        .filter { filter(it, path) }
+        .flatMap { search(it, path + it, filter) }.toSet()
 }
+
--- src/test/kotlin/day12/Day12Test.kt
+++ src/test/kotlin/day12/Day12Test.kt
@@ -1,6 +1,7 @@
 package com.github.rileymichael.day12
 
 import com.github.rileymichael.day12.Day12.part1
+import com.github.rileymichael.day12.Day12.part2
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
@@ -10,7 +11,7 @@ import org.junit.jupiter.params.provider.MethodSource
 class Day12Test {
 
     @ParameterizedTest
-    @MethodSource("input")
+    @MethodSource("part1")
     fun `part one example`(input: String, expected: Int) {
         val actual = Day12.execute(::part1, input)
         Assertions.assertEquals(expected, actual)
@@ -22,9 +23,22 @@ class Day12Test {
         Assertions.assertEquals(3463, actual)
     }
 
+    @ParameterizedTest
+    @MethodSource("part2")
+    fun `part two example`(input: String, expected: Int) {
+        val actual = Day12.execute(::part2, input)
+        Assertions.assertEquals(expected, actual)
+    }
+
+    @Test
+    fun `part two solution`() {
+        val actual = Day12.execute(::part2)
+        Assertions.assertEquals(91533, actual)
+    }
+
     companion object {
         @JvmStatic
-        private fun input() = listOf(
+        private fun part1() = listOf(
             Arguments.of(
                 """
                 start-A
@@ -74,6 +88,60 @@ class Day12Test {
                 start-RW
                 """.trimIndent(),
                 226
+            )
+        )
+
+        @JvmStatic
+        private fun part2() = listOf(
+            Arguments.of(
+                """
+                start-A
+                start-b
+                A-c
+                A-b
+                b-d
+                A-end
+                b-end
+                """.trimIndent(),
+                36
+            ),
+            Arguments.of(
+                """
+                dc-end
+                HN-start
+                start-kj
+                dc-start
+                dc-HN
+                LN-dc
+                HN-end
+                kj-sa
+                kj-HN
+                kj-dc
+                """.trimIndent(),
+                103
+            ),
+            Arguments.of(
+                """
+                fs-end
+                he-DX
+                fs-he
+                start-DX
+                pj-DX
+                end-zg
+                zg-sl
+                zg-pj
+                pj-he
+                RW-he
+                fs-DX
+                pj-RW
+                zg-RW
+                start-pj
+                he-WI
+                zg-he
+                pj-fs
+                start-RW
+                """.trimIndent(),
+                3509
             )
         )
     }
